b3aca6a238e8d5453630cb6f5460a23ef687394b
SUBID DAS <itsmesubid@gmail.com>
2022-10-17 00:18:30 +0530

    Add `authentication` for `users`

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,12 +3,13 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const fetchUser = require("../middleware/fetchUser");
 const router = express.Router();
 require("dotenv").config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-// Create a new user using POST "/api/users"
+// SIGNUP ROUTE: Create a new user using POST "/api/users/signup"
 router.post(
   "/signup",
   [
@@ -18,6 +19,7 @@ router.post(
       min: 8,
     }),
     body("confirmPassword", "Passwords do not match").custom(
+      // This is a custom validator that checks if the password and confirm password fields match
       (value, { req }) => {
         if (value !== req.body.password) {
           throw new Error("Passwords do not match");
@@ -27,6 +29,7 @@ router.post(
     ),
   ],
   async (req, res) => {
+    // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -50,8 +53,13 @@ router.post(
         password: secPass,
       })
         .then((user) => {
-          //   jwt.sign(data,);
-          res.json(user);
+          const data = {
+            user: {
+              id: user.id,
+            },
+          };
+          const authToken = jwt.sign(data, JWT_SECRET);
+          res.json({ authToken });
         })
         .catch((err) =>
           res.status(500).json({
@@ -66,5 +74,64 @@ router.post(
     }
   }
 );
+
+// LOGIN ROUTE: Authenticate a user using POST "/api/users/login"
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 8 characters").isLength({
+      min: 8,
+    }),
+  ],
+  async (req, res) => {
+    // If there are errors, return Bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      ({ email, password } = req.body);
+      let user = await User.findOne({ email: email });
+      // If user does not exist, return bad request and error
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please login with valid email and password" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      // If password does not match, return bad request and error
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please login with valid email and password" });
+      }
+      // Create a new JWT token and return it
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch {
+      console.error(err.message);
+      res.status(500).send("Internal Server Error!\nSomething went wrong!");
+    }
+  }
+);
+
+// GET USER DETAILS ROUTE: Get logged in user details using POST "/api/users/getuser"
+router.post("/getuser", fetchUser, async (req, res) => {
+  try {
+    // Get user details from the database
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error!\nSomething went wrong!");
+  }
+});
 
 module.exports = router;
