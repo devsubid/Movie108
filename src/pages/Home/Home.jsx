7475a527f6a84abbbcfdcd1bec185831c473f7eb
SUBID DAS <itsmesubid@gmail.com>
2022-10-17 19:37:14 +0530

    Clean up `Front-end`

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 function Home() {
+  fetch("http://localhost:5000/api/movies/fetchmovies", {
+    method: "GET",
+    mode: "no-cors",
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
   return (
     <div className="container">
       <div className="home">
