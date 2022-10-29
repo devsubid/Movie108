1578663270c2d1d3542feb1669d537c056af76aa
SUBID DAS <itsmesubid@gmail.com>
2022-10-29 14:36:04 +0530

    Connect to `adaptable`

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/movie108";
+const mongoURI = "mongodb+srv://movie108-main-db-03617103f62:5hdKH93c1d2MpJb1KdUCs4f3WPYcPb@prod-us-central1-1.lfuy1.mongodb.net/movie108-main-db-03617103f62";
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI).then(() => {
