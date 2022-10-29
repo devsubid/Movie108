0ed388e699d76624dabd8f612907760315fb3100
SUBID DAS <itsmesubid@gmail.com>
2022-10-29 12:30:01 +0530

    Organize files

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 connectToMongo();
 const app = express();
-const port = process.env.SERVER_PORT;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
